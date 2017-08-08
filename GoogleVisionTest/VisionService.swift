01a0dfdd16c65984b207486c4e5a9e380b9b643c
Chris Nevin <chris.nevin@tigerspike.com>
2017-08-08 20:57:24 +0100

    Moved service provider to another file.

--- GoogleVisionTest/VisionService.swift
+++ GoogleVisionTest/VisionService.swift
@@ -9,30 +9,6 @@
 import Foundation
 import Moya
 
-let apiKey = ""
-
-struct ServiceProvider {
-    static let shared = ServiceProvider()
-    
-    let provider: RxMoyaProvider<VisionService>
-    
-    private init() {
-        let endpointClosure = { (target: VisionService) -> Endpoint<VisionService> in
-            let defaultEndpoint = MoyaProvider.defaultEndpointMapping(for: target)
-            let endpoint = Endpoint<VisionService>(
-                url: "\(defaultEndpoint.url)?key=\(apiKey)",
-                sampleResponseClosure: defaultEndpoint.sampleResponseClosure,
-                method: target.method,
-                parameters: target.parameters,
-                parameterEncoding: target.parameterEncoding,
-                httpHeaderFields: defaultEndpoint.httpHeaderFields)
-            return endpoint
-        }
-        provider = RxMoyaProvider<VisionService>(endpointClosure: endpointClosure)
-    }
-    
-}
-
 enum VisionService {
     case annotate(ImageResource)
 }
